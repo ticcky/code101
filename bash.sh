53862523cd7455c577a612e0ba6b1e76936718a3
Lukas Zilka <lukas@zilka.me>
2014-10-16 18:31:44 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,2 @@
+# Get the directory of the script. (source: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
