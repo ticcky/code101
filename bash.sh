fa8dee6b25ef45376460f4f1ef91499a5651efe6
Lukas Zilka <lukas@zilka.me>
2014-11-06 16:28:04 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,7 @@
 # Get the directory of the script. (source: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Extract parts of path.
+filename=$(basename "$fullfile")
+extension="${filename##*.}"
+filename="${filename%.*}"
