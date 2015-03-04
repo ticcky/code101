f6b6c98279fb80611375d097a58dbef7a104f472
Lukas Zilka <lukas@zilka.me>
2015-03-04 09:27:13 +0100

    Update argparse.py

--- argparse.py
+++ argparse.py
@@ -4,5 +4,6 @@ argp = argparse.ArgumentParser()
 argp.add_argument('arg1')
 argp.add_argument('--is_good', action='store_true', default=False)
 argp.add_argument('--tag', default='')
+argp.add_argument('--field', action='append')  # Results in a list argument.
 
 args = argp.parse_args()
