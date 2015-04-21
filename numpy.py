886b5254b9da74ad6d6f8d4ed9ff0d2346a53f33
Lukas Zilka <lukas@zilka.me>
2015-04-21 09:49:14 +0200

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -3,3 +3,7 @@ for i in range(x.shape[0]):
     dims = x[:,i].argsort()[-5:]
     vals = x[dims,i]
     print dims, vals
+
+# Better printing options.
+np.set_printoptions(threshold='nan')
+np.set_printoptions(linewidth=200)
